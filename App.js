053316f565c6fb3faa74b1c34f3aa029b9df34f1
raissa-eugenio <eugenio.r@aluno.ifsp.edu.br>
2025-08-05 14:50:23 -0300

    exemplo de aplicação de estilo no text e view

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>G R sempre</Text>
+      <Text>Principal</Text>
+       <Text>Subtitulo</Text>
+        <Text>Paragrafo</Text>
+         <Text>Saiba mais</Text>
       <StatusBar style="auto" />
     </View>
   );
